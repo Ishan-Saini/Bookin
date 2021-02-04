25b35f33753cf3e92ef2d76c18fbc7097d10fa6b
Ishan-Saini <ishansaini243@gmail.com>
2021-02-04 14:08:46 +0530

    server setup

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+const DB = process.env.DATABASE;
+const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => console.log('DB connection successful'));
+
+app.listen(port, () => {
+  console.log(`Listening to requests on port ${port}`);
+});
